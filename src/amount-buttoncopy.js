14d4bcbecf785074996b05b20f9b81c0caa9ea99
JairNeto1 <jairmonteironeto@gmail.com>
2021-09-17 09:19:34 -0300

    upadating the CSS and JS
    Co-authored-by: Luciano Mendes <luciano.auad@gmail.com>

--- src/amount-buttoncopy.js
+++ src/amount-buttoncopy.js
@@ -1,3 +1,24 @@
+var getCustomAmountWrapper = function () {
+  return $('.custom-amount-wrapper')
+}
+
+var getForm = function () {
+  return $('#pwmbForm')
+}
+
+var getWidgetContainer = function () {
+  return $('#widget-container')
+}
+
+var startDynamicWidget = function () {
+  if (getWidgetContainer().length) {
+    var serializedData = getForm().serializeFormJSON()
+    serializedData.isDynamicWidget = true
+    signAndContinue(serializedData, PayWithMyBank.selectBankWidget)
+  }
+}
+
+// Amount buttons
 function cent() {
   document.querySelector('#five').classList.remove('amount-button-selected')
   document.querySelector('#fifty').classList.remove('amount-button-selected')
