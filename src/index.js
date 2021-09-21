070437bd569d6b25591f5785909836fbd9430fd3
JairNeto1 <jairmonteironeto@gmail.com>
2021-09-21 12:19:14 -0300

    udating project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
+function cent() {
+  document.querySelector('#five').classList.remove('amount-button-selected')
+  document.querySelector('#fifty').classList.remove('amount-button-selected')
+  document.querySelector('#custom').classList.remove('amount-button-selected')
+  document.querySelector('.editon').classList.remove('amount-button-selected')
+  document.querySelector('#cent').classList.add('amount-button-selected')
+  document.querySelector('.editon').style.display = 'none'
+  var _cent = document.getElementById('#cent').value
+}
+
+function five() {
+  document.querySelector('#fifty').classList.remove('amount-button-selected')
+  document.querySelector('#custom').classList.remove('amount-button-selected')
+  document.querySelector('#cent').classList.remove('amount-button-selected')
+  document.querySelector('#five').classList.add('amount-button-selected')
+  document.querySelector('.editon').style.display = 'none'
+}
+
+function fifty() {
+  document.querySelector('#five').classList.remove('amount-button-selected')
+  document.querySelector('#custom').classList.remove('amount-button-selected')
+  document.querySelector('#cent').classList.remove('amount-button-selected')
+  document.querySelector('#fifty').classList.add('amount-button-selected')
+  document.querySelector('.editon').style.display = 'none'
+}
+
+function custom() {
+  document.querySelector('#five').classList.remove('amount-button-selected')
+  document.querySelector('#fifty').classList.remove('amount-button-selected')
+  document.querySelector('#cent').classList.remove('amount-button-selected')
+  document.querySelector('#custom').classList.add('amount-button-selected')
+  document.querySelector('.editon').style.display = 'block'
+}
+
+// widget
+
 var PayWithMyBankOptions = {
   closeButton: true,
   dragAndDrop: true,
@@ -7,24 +43,28 @@ var PayWithMyBankOptions = {
 var establishData = {
   accessId: 'XHC67YWPe29WVg92EyZP',
   merchantId: '1017088088',
-  merchantReference: 'Money2020',
+  merchantReference: Math.random(),
   description: 'Money2020',
+  //showSuccessMessage: true,
+  //successMessage: 'This transaction has been completed.',
   currency: 'USD',
-  amount: '0.50',
+  amount: '5.00',
   paymentType: 'Deferred',
   returnUrl: '#success',
   cancelUrl: '#cancel'
 }
 
+//Callback validation
+
 PayWithMyBank.addPanelListener(function (command, event) {
   if (command === 'event' && event.type === 'new_location') {
     if (event.data.indexOf('#success') === 0) {
       //location = 'https://trustlyintegration.herokuapp.com/'
-      location = 'http://127.0.0.1:5501'
+      location = 'http://127.0.0.1:5502'
       console.log('[success]', event.data)
     } else {
       console.log('[cancel]', event.data)
-      location = 'http://127.0.0.1:5501'
+      location = 'http://127.0.0.1:5502'
     }
     return false
   }
