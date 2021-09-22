565b5eef7a14f24c9d7f87eed93e8f10c5c1bef4
JairNeto1 <jairmonteironeto@gmail.com>
2021-09-22 15:02:58 -0300

    refactioring the HTM and CSS

--- src/index.js
+++ src/index.js
@@ -54,11 +54,14 @@ var establishData = {
   cancelUrl: '#cancel'
 }
 
+//Confirmation page
+
 //Callback validation
 
 PayWithMyBank.addPanelListener(function (command, event) {
   if (command === 'event' && event.type === 'new_location') {
     if (event.data.indexOf('#success') === 0) {
+      document.querySelector('.modal-container').style.display = 'flex'
       //location = 'https://trustlyintegration.herokuapp.com/'
       location = 'http://127.0.0.1:5502'
       console.log('[success]', event.data)
